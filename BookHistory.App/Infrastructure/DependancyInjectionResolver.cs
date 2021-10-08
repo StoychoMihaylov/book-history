34fb509c1725127301ece67db79bfdf616cef07b
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2021-10-08 22:03:53 +0200

    fead: database initial scheme and db context infrastructure

---
+++ BookHistory.App/Infrastructure/DependancyInjectionResolver.cs
@@ -0,0 +1,16 @@
+﻿namespace BookHistory.App.Infrastructure
+{
+    using BookHistory.Data.Context;
+    using BookHistory.Data.Interfaces;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependancyInjectionResolver
+    {
+        public static IServiceCollection AddDependancyInjectionResolver(this IServiceCollection services)
+        {
+            services.AddTransient<IBookHistoryDbContext, BookHistoryDbContext>();
+            
+            return services;
+        }
+    }
+}
---
+++ BookHistory.App/Infrastructure/PosgreSQLWithEntityFrameworkExtesion.cs
@@ -0,0 +1,23 @@
+﻿namespace BookHistory.App.Infrastructure
+{
+    using Microsoft.EntityFrameworkCore;
+    using BookHistory.Data.Context;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class PosgreSQLWithEntityFrameworkExtesion
+    {
+        public static IServiceCollection AddPosgreSQLWithEntityFramework(this IServiceCollection services, IConfiguration configuration)
+        {
+            services
+                .AddEntityFrameworkNpgsql()
+                .AddDbContext<BookHistoryDbContext>((sp, opt) =>
+                {   
+                    opt.UseNpgsql(configuration.GetConnectionString("BookHistoryDB"))
+                        .UseInternalServiceProvider(sp);
+                });
+
+            return services;
+        }
+    }
+}
