cbcafa90591333a6bc194dcfe19437bfb83cdca0
StoychoMihaylov <st.mihaylov.mihaylov@gmail.com>
2021-10-10 01:39:34 +0200

    fead: create-book improvements(autocomplete for already existing author names)

---
+++ BookHistory.App/Controllers/AuthorController.cs
@@ -0,0 +1,28 @@
+﻿namespace BookHistory.App.Controllers
+{
+    using BookHistory.Services.Interfaces;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System.Threading.Tasks;
+
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        private readonly ILogger<AuthorController> logger;
+        private readonly IAuthorService authorService;
+
+        public AuthorController(ILogger<AuthorController> logger, IAuthorService authorService)
+        {
+            this.logger = logger;
+            this.authorService = authorService;
+        }
+
+        [HttpGet]
+        [Route("authors")]
+        public async Task<IActionResult> GetListOfAuthorNames()
+        {
+            var authorNames = await this.authorService.GetListOfAuthorNames();
+            return StatusCode(201, authorNames);
+        }
+    }
+}
